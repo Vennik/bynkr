0e2f2511f6a88afa575dcbe36f96d501937f814e
Jasper Boot <mrjasperboot@gmail.com>
2016-03-22 23:43:52 +0100

    added useless shit

--- navigation.php
+++ navigation.php
@@ -5,6 +5,9 @@
         </div>
         <div id="navbar" class="navbar-right">
             <ul class="nav navbar-nav">
+                <?php if (isset($_SESSION['email'])): ?>
+                    <li class="navbar-text">Ingelogd als <?= $_SESSION['email'] ?></li>
+                <?php endif ?>
                 <li><a href="#"><span class="glyphicon glyphicon-cog"></a></li>
             </ul>
         </div>
