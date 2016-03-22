4fa9326259f24b6aee3375434009495a6db1e41e
René Vennik <renevennik@gmail.com>
2016-03-22 21:38:49 +0100

    Messages.

--- documents.php
+++ documents.php
@@ -23,26 +23,91 @@
 
 <div class="container theme-showcase" role="main">
 
-    <div class="documents">
+    <!-- Row -->
+    <div class="row">
 
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>a-very-long-folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>a-very-long-folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>a-very-long-folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>folder</p></a>
-        <a href="#" class="document"><span class="glyphicon glyphicon-folder-open"></span><br><p>a-very-long-folder</p></a>
+        <!-- Col -->
+        <div class="col-md-8">
+
+            <div class="documents">
+
+                <div class="page-header">
+                    <h2>ING documents</h2>
+                </div>
+
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+
+                <div class="page-header">
+                    <h2>Personal documents</h2>
+                </div>
+
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>filename.png</p></a>
+                <a href="#" class="document"><span class="glyphicon glyphicon-file"></span><br><p>a-very-long-filename.png</p></a>
+
+            </div>
+
+        </div>
+        <!-- End Col -->
+
+        <!-- Col -->
+        <div class="col-md-4">
+
+            <div class="messages">
+
+                <div class="page-header">
+                    <h2>Correspondence</h2>
+                </div>
+
+                <div class="between">
+                    January 2016
+                </div>
+                <a href="#" class="message">
+                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                    <p>28 January</p>
+                </a>
+                <a href="#" class="message">
+                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                    <p>21 January</p>
+                </a>
+
+                <div class="between">
+                    2015
+                </div>
+                <a href="#" class="message">
+                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                    <p>21 November</p>
+                </a>
+                <a href="#" class="message">
+                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                    <p>16 August</p>
+                </a>
+
+                <div class="between">
+                    2014
+                </div>
+                <a href="#" class="message">
+                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                    <p>8 June</p>
+                </a>
+
+            </div>
+
+        </div>
+        <!-- End Col -->
 
     </div>
+    <!-- End Row -->
 
 </div>
 
