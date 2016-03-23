d5bd5d47bba782f0a91b82ea76a5231a4227c41e
jnieuwdorp <jaspernieuwdorp@hotmail.com>
2016-03-23 04:25:21 +0100

    added upload button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(function () {
     $('.main-menu > li > a.dropdown').click(setActive);
+    $('.upload').click(addFile);
+
+    function addFile(){
+        var file = document.getElementById("selected-file").value;
+        document.getElementById('placeholder').innerHTML = '<a href="#" class="document"><span class="glyphicon glyphicon glyphicon-file"></span><br><p>'+file+'</p>';
+        alert(file);
+    }
 
     function setActive() {
         var item = $(this).parent(),
@@ -15,4 +22,27 @@ $(function () {
             item.addClass('active');
         }
     }
+
+    $(document).on('change', '.btn-file :file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
+
+    $(document).ready( function() {
+        $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+
+            var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+            if( input.length ) {
+                input.val(log);
+            } else {
+                if( log ) alert(log);
+            }
+
+        });
+    });
+
 });
--- pages/vault.php
+++ pages/vault.php
@@ -1,11 +1,12 @@
 <ul class="main-menu">
     <li>
-        <a href="overview" class="dropdown" onclick="return false;"><span class="fa fa-shopping-cart"></span>Products</a>
+        <a href="overview" class="dropdown" onclick="return false;"><span
+                class="fa fa-shopping-cart"></span>Products</a>
         <ul class="submenu">
             <?php
 
             foreach ($pages as $page) {
-                echo '<li' . (isset($components[1]) && $components[1] == $page['url'] ? ' class="active"' : '') . '><a href="overview/' . $page['url'] . '">'.$page['name'].'</a></li>';
+                echo '<li' . (isset($components[1]) && $components[1] == $page['url'] ? ' class="active"' : '') . '><a href="overview/' . $page['url'] . '">' . $page['name'] . '</a></li>';
             }
 
             ?>
@@ -22,88 +23,126 @@
         <!-- Container -->
         <div class="container-fluid">
 
-        <!-- Col -->
-        <div class="col-md-8">
+            <!-- Col -->
+            <div class="col-md-8">
 
 
-            <div class="documents">
+                <div class="documents">
 
-                <div class="page-header">
-                    <h2>Personal vault</h2>
+                    <div class="page-header">
+                        <h2>Personal vault</h2>
+                    </div>
+
+                    <a href="#" class="document"><span class="glyphicon glyphicon-bitcoin"></span><br>
+                        <p>wallet.dat</p></a>
+                    <a href="#" class="document"><span class="glyphicon glyphicon glyphicon-lock"></span><br>
+                        <p>id_rsa</p></a>
+
+                    <?php
+
+                    $icons = ['glyphicon-file', 'glyphicon-tag', 'glyphicon-briefcase', 'glyphicon-euro'];
+                    $files = ['filename.png', 'a-very-long-filename.png', 'test.jpg'];
+                    $amount = rand(1, 2);
+
+                    for ($i = 0; $i < $amount; $i++) {
+                        echo '<a href="#" class="document"><span class="glyphicon ' . $icons[array_rand($icons)] . '"></span><br><p>' . $files[array_rand($files)] . '</p></a>';
+                    }
+
+                    ?>
+                    <!--for file upload -->
+                    <div class="documents" id="placeholder"></div>
+
+                    <a href="#" class="document uploadbutton buzz" data-toggle="modal" data-target="#myModal"><span
+                            class="glyphicon glyphicon glyphicon-plus"></span><br>
+                        <p>Add file</p></a>
+
+                    <!--End for file upload -->
+                    <div class="clear"></div>
+
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Upload a file</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="input-group">
+                                        <span class="input-group-btn">
+                                            <span class="btn btn-primary btn-file">
+                                               <input type="file" multiple> Browse
+                                            </span>
+                                        </span>
+                                        <input type="text" class="form-control" id="selected-file" readonly>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-success upload" data-dismiss="modal">Upload
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
                 </div>
-
-                <a href="#" class="document"><span class="glyphicon glyphicon-bitcoin"></span><br><p>wallet.dat</p></a>
-                <a href="#" class="document"><span class="glyphicon glyphicon glyphicon-lock"></span><br><p>id_rsa</p></a>
-
-                <?php
-
-                $icons = ['glyphicon-file', 'glyphicon-tag', 'glyphicon-briefcase', 'glyphicon-euro'];
-                $files = ['filename.png', 'a-very-long-filename.png', 'test.jpg'];
-                $amount = rand(1, 2);
-
-                for ($i = 0; $i < $amount; $i++) {
-                    echo '<a href="#" class="document"><span class="glyphicon ' . $icons[array_rand($icons)] . '"></span><br><p>' . $files[array_rand($files)] . '</p></a>';
-                }
-
-                ?>
-
-                <div class="clear"></div>
-
-
 
             </div>
+            <!-- End Col -->
 
-        </div>
-        <!-- End Col -->
+            <!-- Col -->
+            <div class="col-md-4">
 
-        <!-- Col -->
-        <div class="col-md-4">
+                <div class="messages-side">
 
-            <div class="messages-side">
+                    <div class="page-header">
+                        <h2>Correspondence</h2>
+                    </div>
 
-                <div class="page-header">
-                    <h2>Correspondence</h2>
+                    <div class="between">
+                        January 2016
+                    </div>
+                    <a href="#" class="message">
+                        <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                        <p>28 January</p>
+                    </a>
+                    <a href="#" class="message">
+                        <h4><span class="glyphicon glyphicon-comment"></span> An other message</h4>
+                        <p>21 January</p>
+                    </a>
+
+                    <div class="between">
+                        2015
+                    </div>
+                    <a href="#" class="message">
+                        <h4><span class="glyphicon glyphicon-comment"></span> Message</h4>
+                        <p>21 November</p>
+                    </a>
+                    <a href="#" class="message">
+                        <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                        <p>16 August</p>
+                    </a>
+
+                    <div class="between">
+                        2014
+                    </div>
+                    <a href="#" class="message">
+                        <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
+                        <p>8 June</p>
+                    </a>
+
                 </div>
-
-                <div class="between">
-                    January 2016
-                </div>
-                <a href="#" class="message">
-                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
-                    <p>28 January</p>
-                </a>
-                <a href="#" class="message">
-                    <h4><span class="glyphicon glyphicon-comment"></span> An other message</h4>
-                    <p>21 January</p>
-                </a>
-
-                <div class="between">
-                    2015
-                </div>
-                <a href="#" class="message">
-                    <h4><span class="glyphicon glyphicon-comment"></span> Message</h4>
-                    <p>21 November</p>
-                </a>
-                <a href="#" class="message">
-                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
-                    <p>16 August</p>
-                </a>
-
-                <div class="between">
-                    2014
-                </div>
-                <a href="#" class="message">
-                    <h4><span class="glyphicon glyphicon-comment"></span> Title of message</h4>
-                    <p>8 June</p>
-                </a>
 
             </div>
+            <!-- End Col -->
 
         </div>
-        <!-- End Col -->
-
-    </div>
-    <!-- End Row -->
+        <!-- End Row -->
 
     </div>
     <!-- End Container -->
