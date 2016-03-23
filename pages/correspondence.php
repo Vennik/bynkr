7fec425b52ffb2bf984f7c62e78c1a4778ac9fdf
Jasper Boot <mrjasperboot@gmail.com>
2016-03-23 08:36:01 +0100

    improved hacky code by nieuwdorp

--- pages/correspondence.php
+++ pages/correspondence.php
@@ -45,36 +45,43 @@
                             <td>23-3-2016</td>
                             <td>18:00</td>
                             <td><span class="label label-success">Whatsapp</span></td>
-                            <td><a class="refrecconv">Refinancing my house</a> <span class="badge">4</span></td>
+                            <td>
+                                <a class="refrecconv">Refinancing my house</a>
+                                <span class="badge">4</span>
+                            </td>
                         </tr>
                         <tr class="odd">
                             <td>22-3-2016</td>
                             <td>19:00</td>
                             <td><span class="label label-primary">Facebook</span></td>
-                            <td><a class="refrecconv">What to do if I lose my phone while abroad?</a> <span
-                                    class="badge">12</span></td>
+                            <td>
+                                <a class="refrecconv">What to do if I lose my phone while abroad?</a>
+                                <span class="badge">12</span>
+                            </td>
                         </tr>
                         <tr class="even">
                             <td>22-3-2016</td>
                             <td>20:00</td>
                             <td><span class="label label-default">Call</span></td>
-                            <td><a class="refrecconv">Question: How do I change my PIN?</a> <span class="badge">1</span>
+                            <td>
+                                <a class="refrecconv">Question: How do I change my PIN?</a>
+                                <span class="badge">1</span>
                             </td>
                         </tr>
                         <tr class="odd">
                             <td>20-3-2016</td>
                             <td>12:13</td>
                             <td><span class="label label-warning">ING app</span></td>
-                            <td><a class="refrecconv">Feedback: I love the app <span class="badge">3</span></td>
+                            <td>
+                                <a class="refrecconv">Feedback: I love the app</a>
+                                <span class="badge">3</span>
+                            </td>
                         </tr>
 
                         <?php
-                        function getCor($odd)
+                        function getCor()
                         {
                             $platforms = [['Facebook', 'primary'], ['Whatsapp', 'success'], ['ING app', 'warning'], ['Call', 'default']];
-                            $tuple = $platforms[array_rand($platforms)];
-                            $platform = $tuple[0];
-                            $color = $tuple[1];
                             $day = rand(1, 29);
                             $month = rand(1, 9);
                             $hour = rand(1, 24);
@@ -87,7 +94,11 @@
                             $text = array_filter(explode("\n", wordwrap($text, 80)));
                             $last = null;
                             $count = 1;
+                            $even = true;
                             foreach ($text as $t) {
+                                $tuple = $platforms[array_rand($platforms)];
+                                $platform = $tuple[0];
+                                $color = $tuple[1];
                                 $rand = rand(1, 2);
                                 if ($last === $rand) {
                                     if ($count < 1) {
@@ -101,21 +112,21 @@
                                 }
                                 $last = $rand;
                                 $minutes += rand(1, 5);
+
+                                echo '<tr class=' . ($even ? 'even' : 'odd') . '>
+                                    <td>' . $date . '</td>
+                                    <td>' . $time . '</td>
+                                    <td><span class="label label-' . $color . '">' . $platform . '</span></td>
+                                    <td>
+                                        <a class="refrecconv">' . $t . '</a>
+                                        <span class="badge">' . $messages . '</span>
+                                    </td>
+                                </tr>';
                             }
 
-                            echo '<tr class=' . $odd . '>
-                                <td>' . $date . '</td>
-                                <td>' . $time . '</td>
-                                <td><span class="label label-' . $color . '">' . $platform . '</span></td>
-                                <td><a class = "refrecconv">' . $t . '</a> <span class="badge">' . $messages . '</span></td>
-                            </tr>';
                         }
 
-                        $amount = rand(3, 8);
-                        for ($i = 0; $i < $amount; $i++) {
-                            getCor("even");
-                            getCor("odd");
-                        }
+                        getCor();
                         ?>
                         </tbody>
                     </table>
