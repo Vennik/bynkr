ebfdbca986b185d2b324190e3b128ee03e808ea0
jnieuwdorp <jaspernieuwdorp@hotmail.com>
2016-03-23 09:31:23 +0100

    added edit page

---
+++ pages/edit.php
@@ -0,0 +1,78 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
+        </div>
+        <div
+            class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
+
+
+            <div class="panel panel-info">
+                <div class="panel-own">
+                    <h3 class="panel-title">Gina Beuk</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3 col-lg-3 " align="center"><img alt="User Pic"
+                                                                            src="./images/customer3.png"
+                                                                            class="img-circle img-responsive"></div>
+
+                        <!--<div class="col-xs-10 col-sm-10 hidden-md hidden-lg"> <br>
+                          <dl>
+                            <dt>DEPARTMENT:</dt>
+                            <dd>Administrator</dd>
+                            <dt>HIRE DATE</dt>
+                            <dd>11/12/2013</dd>
+                            <dt>DATE OF BIRTH</dt>
+                               <dd>11/12/2013</dd>
+                            <dt>GENDER</dt>
+                            <dd>Male</dd>
+                          </dl>
+                        </div>-->
+                        <div class=" col-md-9 col-lg-9 ">
+                            <table class="table table-user-information">
+                                <tbody>
+                                <td>Date of Birth</td>
+                                <td>01/24/1988</td>
+                                </tr>
+
+                                <tr>
+                                <tr>
+                                    <td>Gender</td>
+                                    <td>Female</td>
+                                </tr>
+                                <tr>
+                                    <td>Twitter Handle</td>
+                                    <td><a href="#">@GinaBeuk</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Facebook</td>
+                                    <td><a href="#">fb.com/GinaBeuk</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Email</td>
+                                    <td><a href="#">GinaBeuk@outlook.com</a></td>
+                                </tr>
+                                <td>Phone Number</td>
+                                <td>123-4567-890(Landline)<br><br>555-4567-890(Mobile)
+                                </td>
+
+                                </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                     <span class="pull-right">
+                            <a href="#"
+                               class="btn btn-orange"><i class="glyphicon glyphicon-edit"></i> Edit</a>
+                        </span>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
