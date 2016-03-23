28797b399a80bf3406c8c8353700fb9a02960e32
Jasper Boot <mrjasperboot@gmail.com>
2016-03-23 01:54:48 +0100

    shorter code

--- pages/correspondence.php
+++ pages/correspondence.php
@@ -76,56 +76,56 @@
                     <div class="page-header">
                         <h2>Conversation</h2>
                     </div>
-                        <div class="panel panel-default">
-                            <div class="panel-body msg_container_base">
-                                <?php
-                                $minutes = 9;
-                                $last = null;
+                    <div class="panel panel-default">
+                        <div class="panel-body msg_container_base">
+                            <?php
+                            $minutes = 9;
+                            $last = null;
+                            $count = 0;
+                            $text = file_get_contents('http://loripsum.net/api/1/verylong/plaintext');
+                            $text = explode("\n", wordwrap($text, 80));
+                            for ($i = 0;
+                            $i < 10;
+                            $i++):
+                            $rand = rand(0, 1);
+                            if ($last === $rand) {
+                                if ($count < 1) {
+                                    $count += 1;
+                                } else {
+                                    $count = 0;
+                                    $rand = 1 - $rand;
+                                }
+                            } else {
                                 $count = 0;
-                                $text = file_get_contents('http://loripsum.net/api/1/verylong/plaintext');
-                                $text = explode("\n", wordwrap($text, 80));
-                                for ($i = 0; $i < 10; $i++):
-                                    $rand = rand(0, 1);
-                                    if ($last === $rand) {
-                                        if ($count < 1) {
-                                            $count += 1;
-                                        } else {
-                                            $count = 0;
-                                            $rand = 1 - $rand;
-                                        }
-                                    } else {
-                                        $count = 0;
-                                    }
-                                    $last = $rand;
-                                    $minutes += rand(0, 5);
-                                    if ($rand): ?>
-                                        <div class="row msg_container base_sent">
-                                            <div class="col-md-10 col-xs-10">
-                                                <div class="messages msg_sent">
-                                                    <p><?= $text[$i] ?></p>
-                                                    <time datetime="2009-11-13T20:00">Randy • <?= $minutes ?> min</time>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-2 col-xs-2 avatar">
-                                                <img src="images/customer.png" class="img-responsive">
-                                            </div>
-                                        </div>
-                                    <?php else: ?>
-                                        <div class="row msg_container base_receive">
-                                            <div class="col-md-2 col-xs-2 avatar">
-                                                <img src="images/logo.png" class="img-responsive">
-                                            </div>
-                                            <div class="col-md-10 col-xs-10">
-                                                <div class="messages msg_receive">
-                                                    <p><?= $text[$i] ?></p>
-                                                    <time datetime="2009-11-13T20:00">Timothy • <?= $minutes ?> min</time>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <?php endif;
-                                endfor ?>
+                            }
+                            $last = $rand;
+                            $minutes += rand(0, 5);
+                            ?>
+                            <div class="row msg_container base_sent">
+                                <?php if (!$rand): ?>
+                                    <div class="col-md-2 col-xs-2 avatar">
+                                        <img src="images/logo.png" class="img-responsive">
+                                    </div>
+                                <?php endif ?>
+                                <div class="col-md-10 col-xs-10">
+                                    <div
+                                        class="messages msg_<?php if ($rand): ?>sent<?php else: ?>receive<?php endif ?>">
+                                        <p><?= $text[$i] ?></p>
+                                        <time
+                                            datetime="2009-11-13T20:00"><?php if ($rand): ?>Randy<?php else: ?>Timothy<?php endif ?>
+                                            • <?= $minutes ?> min
+                                        </time>
+                                    </div>
+                                </div>
+                                <?php if ($rand): ?>
+                                    <div class="col-md-2 col-xs-2 avatar">
+                                        <img src="images/customer.png" class="img-responsive">
+                                    </div>
+                                <?php endif ?>
+                            </div>
+                            <?php endfor ?>
 
-                                <!--endrepeat -->
+                            <!--endrepeat -->
 
 
                         </div>
