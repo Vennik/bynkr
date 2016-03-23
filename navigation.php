5940587184691503d55d661fe80e86dfaa490004
jnieuwdorp <jaspernieuwdorp@hotmail.com>
2016-03-23 01:04:44 +0100

    changed language

--- navigation.php
+++ navigation.php
@@ -6,7 +6,7 @@
         <div id="navbar" class="navbar-right">
             <ul class="nav navbar-nav">
                 <?php if (isset($_SESSION['email'])): ?>
-                    <li class="navbar-text">Ingelogd als <?= $_SESSION['email'] ?></li>
+                    <li class="navbar-text">Logged in as <?= $_SESSION['email'] ?></li>
                 <?php endif ?>
                 <li><a href="#"><span class="glyphicon glyphicon-cog"></a></li>
             </ul>
